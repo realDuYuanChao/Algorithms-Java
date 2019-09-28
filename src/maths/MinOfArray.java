6304895674a38d14015ba5c97c7ba104dca4197c
shellhub <shellhub.me@gmail.com>
2019-09-28 21:28:47 +0800

    add author

--- src/maths/MinOfArray.java
+++ src/maths/MinOfArray.java
@@ -1,5 +1,8 @@
 package maths;
 
+/**
+ * @author https://github.com/shellhub
+ */
 public class MinOfArray {
     public static void main(String[] args) {
         int[] array = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
