b8710d77fabc05a5bc2e63f03922249f2e43ae76
shellhub <shellhub.me@gmail.com>
2019-10-01 13:22:17 +0800

    TowerOfHanio

---
+++ src/maths/TowerOfHanoi.java
@@ -0,0 +1,28 @@
+
+package maths;
+
+/**
+ * @author https://github.com/shellhub
+ */
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        char A = 'A';
+        char B = 'B';
+        char C = 'C';
+
+        hanoi(n, A, B, C);
+    }
+
+    public static void hanoi(int n, char A, char B, char C) {
+        if (n == 0) {
+            return;
+        }
+        hanoi(n - 1, A, C, B);
+        System.out.format("Move %c to %c\n", A, C);
+        hanoi(n - 1, B, A, C);
+    }
+}
