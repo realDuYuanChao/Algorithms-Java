5ce9e5e207490b9f19170ae7698dadfa654bb62b
shellhub <shellhub.me@gmail.com>
2019-10-04 00:49:48 +0800

    LeapYear

---
+++ src/maths/LeapYear.java
@@ -0,0 +1,24 @@
+package maths;
+
+/**
+ * @author shellhub
+ * <p>
+ * reference: https://en.wikipedia.org/wiki/Leap_year
+ * </p>
+ */
+public class LeapYear {
+    public static void main(String[] args) {
+        int[] years = {1977, 2021, 1980, 2020, 1900, 2000, 3200};
+        for (int year : years) {
+            if (isLeapYear(year)) {
+                System.out.println(year + " is leap year.");
+            } else {
+                System.out.println(year + " is not leap year.");
+            }
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 3200 != 0);
+    }
+}
