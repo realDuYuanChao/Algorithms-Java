e01fbfa3d78d43946cd79003a017104130f6ecc4
shellhub <shellhub.me@gmail.com>
2019-10-18 19:45:24 +0800

    CheckIPInvalid

---
+++ src/main/java/maths/CheckIPInvalid.java
@@ -0,0 +1,33 @@
+package maths;
+
+public class CheckIPInvalid {
+    public static void main(String[] args) {
+        assert isValid("128.0.0.1");
+        assert isValid("125.16.100.1");
+        assert !isValid("125.512.100.1");
+        assert !isValid("125.512.100.abc");
+    }
+
+    /**
+     * Check if ip is valid
+     *
+     * @param ip to be checked
+     * @return <tt>true</tt> if <tt>ip</tt> is valid, otherwise <tt>false</tt>
+     */
+    private static boolean isValid(String ip) {
+        String[] bits = ip.split("\\.");
+        if (bits.length != 4) {
+            return false;
+        }
+        for (String bit : bits) {
+            try {
+                if (Integer.valueOf(bit) < 0 || Integer.valueOf(bit) > 255) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                return false; /* contains other other character */
+            }
+        }
+        return true;
+    }
+}
