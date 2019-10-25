6370ed8e14a9fc6e3ae3a1880419fbebf2a2df59
shellhub <shellhub.me@gmail.com>
2019-10-25 21:27:25 +0800

    ValidPalindromeII.java

---
+++ src/main/java/leetcode/ValidPalindromeII.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import maths.PalindromeStringRecursion;
+
+/**
+ * link: https://leetcode.com/problems/valid-palindrome-ii/
+ * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+ * <p>
+ * Example 1:
+ * Input: "aba"
+ * Output: True
+ * Example 2:
+ * Input: "abca"
+ * Output: True
+ * Explanation: You could delete the character 'c'.
+ * Note:
+ * The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+ */
+public class ValidPalindromeII {
+    public static void main(String[] args) {
+        assert validPalindrome("aba");
+        assert validPalindrome("abca");
+        assert !validPalindrome("aaabbb");
+        assert !validPalindrome("abc");
+    }
+
+    public static boolean validPalindrome(String s) {
+        int l = -1, r = s.length();
+        while (++l < --r)
+            if (s.charAt(l) != s.charAt(r)) return isPalindromic(s, l, r+1) || isPalindromic(s, l-1, r);
+        return true;
+    }
+
+    public static boolean isPalindromic(String s, int l, int r) {
+        while (++l < --r)
+            if (s.charAt(l) != s.charAt(r)) return false;
+        return true;
+    }
+}
