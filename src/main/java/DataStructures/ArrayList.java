5dfaa2c8bc8041bad6162de2f3bb7c52a710610d
shellhub <shellhub.me@gmail.com>
2019-11-06 22:30:58 +0800

    ArrayList

--- src/main/java/DataStructures/ArrayList.java
+++ src/main/java/DataStructures/ArrayList.java
@@ -1,5 +1,7 @@
 package DataStructures;
 
+import java.util.Arrays;
+
 public class ArrayList {
 
     /* the array contains elements */
@@ -56,10 +58,19 @@ public class ArrayList {
      * @return <tt>true</tt> if add successfully
      */
     public boolean add(int element) {
-        return add(0, element);
+        return add(size, element);
     }
 
+    /**
+     * Add element at specified index
+     *
+     * @param index   the index to add
+     * @param element the element to add
+     * @return {@code true} if add successfully
+     * @throws IndexOutOfBoundsException if {@code index} is invalid
+     */
     public boolean add(int index, int element) {
+        increase();
         checkBoundsForAdd(index);
 
         /* shift all element forward */
@@ -71,19 +82,100 @@ public class ArrayList {
         return true;
     }
 
-    /**
-     * Check bounds for add operation
-     *
-     * @param index to be checked
-     */
-    private void checkBoundsForAdd(int index) {
-        if (index > size || index < 0) {
-            throw new IndexOutOfBoundsException("invalid index:" + index + "for add");
+    public void increase() {
+        if (size == capacity) {
+            capacity = (int) (capacity * 1.5);
+            int[] newItems = new int[capacity];
+            System.arraycopy(items, 0, newItems, 0, size);
+            items = newItems;
         }
     }
 
+    /**
+     * Remove last element at array
+     *
+     * @return the last element at array
+     */
+    public int remove() {
+        return remove(size - 1);
+    }
+
+    /**
+     * Remove element at specified index
+     *
+     * @param index the index of the element to be removed
+     * @return removed element
+     * @throws IndexOutOfBoundsException if {@code is invalid} or array is empty
+     */
+    public int remove(int index) {
+        checkBoundForRemove(index);
+
+        int removed = items[index];
+        for (int i = index; i < size; ++i) {
+            items[i] = items[i + 1]; /* shift all backward */
+        }
+        size--;
+        return removed;
+    }
+
+    /**
+     * Remove all elements of array
+     */
+    public void removeAll() {
+        size = 0;
+    }
+
+    /**
+     * Check bounds for add operation
+     *
+     * @param index to be checked for adding
+     */
+    private void checkBoundsForAdd(int index) {
+        if (index > size || index < 0) {
+            errorMessage(index);
+        }
+    }
+
+    /**
+     * Check bounds for remove
+     *
+     * @param index to be checked for removing
+     */
+    private void checkBoundForRemove(int index) {
+        if (index > size - 1 || index < 0) {
+            errorMessage(index);
+        }
+    }
+
+    private void errorMessage(int index) {
+        throw new IndexOutOfBoundsException("invalid index: " + index + " size: " + size);
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for (int i = 0; i < size; ++i) {
+            builder.append(items[i]).append(", ");
+        }
+        return builder.replace(builder.length() - 2, builder.length(), "]").toString();
+    }
 
     public static void main(String[] args) {
-        
+        ArrayList list = new ArrayList();
+        for (int i = 1; i <= 5; ++i) {
+            list.add(i);
+        }
+
+        assert list.toString().equals("[1, 2, 3, 4, 5]");
+        assert list.remove() == 5;
+        assert list.remove(1) == 2;
+        assert list.toString().equals("[1, 3, 4]");
+
+        list.clear();
+        assert list.toString().equals("[]");
     }
 }
